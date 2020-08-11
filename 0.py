b6d25239bf041b1e5bdc093781a48b01730248dc
GiRiKoTTe <13247441+GiRiKoTTe@users.noreply.github.com>
2020-08-11 19:08:01 +0530

    Update 0.py

--- 0.py
+++ 0.py
@@ -2,13 +2,15 @@
 
 quick_setup_main_rounding = 1
 quick_setup_aux_rounding = 1
-quick_setup_target_percent = [50.0,52.5,55.0,57.5,60.0,62.5,65.0,67.5,70.0,72.5,75.0,77.5,80.0,82.5,85.0,87.5,90.0,92.5,95.0,97.5,100.0
+quick_setup_main_target_percent = [50.0,52.5,55.0,57.5,60.0,62.5,65.0,67.5,70.0,72.5,75.0,77.5,80.0,82.5,85.0,87.5,90.0,92.5,95.0,97.5,100.0
+]
+quick_setup_aux_target_percent = [50.0,52.5,55.0,57.5,60.0,62.5,65.0,67.5,70.0,72.5,75.0,77.5,80.0,82.5,85.0,87.5,90.0,92.5,95.0,97.5,100.0
 ]
 #Main
 quick_setup_main = "main"
 quick_setup_main_max = 400
 quick_setup_main_single_at_8 = 90
-quick_setup_main_behavior_RIR_sets = 5
+quick_setup_main_behavior_RIR_sets = [5]*21
 quick_setup_main_behavior_RIR_adjust =      [-5,-2,0,.5,1,1.5,2,3]
 quick_setup_main_rep_target =       [8,8,8,8,7,7,6,6,5,5,4,4,3,3,2,2,1,1,1,1,1]
 quick_setup_main_last_set_RIR_target =      [3,3,3,3,3,3,3,3,3,3,2,2,2,2,1,1,1,1,1,0,0]
@@ -30,36 +32,103 @@ quick_setup_aux_intensity =    [60.0,65.0,70.0,62.5,67.5,72.5,50.0,65.0,70.0,75.
 
 setup_main_intensity = quick_setup_main_intensity
 setup_main_sets = quick_setup_main_behavior_RIR_sets
-def setup_main_behavior_RIR_adjust(w):
-  if w == 1:
-    return quick_setup_main_behavior_RIR_adjust
-  elif w >= 2 and w<= 21:
-    return setup_main_behavior_RIR_adjust(w-1)
+setup_main_behavior_RIR_adjust = quick_setup_main_behavior_RIR_adjust
 
 def setup_main_reps_per_set (w):
-    if setup_main_intensity[w-1] = quick_setup_main_rep_target[w-1]:
-      return 
-def setup_main_reps_per_set (w):
-  count=1
-  while 1<=count<=21:
-
-      if setup_main_intensity[w - 1] == quick_setup_main_intensity[count - 1]:
-          setup_main_reps_per_set = quick_setup_main_rep_target[count - 1]
-          break
-      else :
-          count+=1
-  return setup_main_reps_per_set
+    if setup_main_intensity[w-1] == quick_setup_main_target_percent[0]:
+      return quick_setup_main_rep_target[0]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[1]:
+      return quick_setup_main_rep_target[1]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[2]:
+      return quick_setup_main_rep_target[2]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[3]:
+      return quick_setup_main_rep_target[3]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[4]:
+      return quick_setup_main_rep_target[4]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[5]:
+      return quick_setup_main_rep_target[5]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[6]:
+      return quick_setup_main_rep_target[6]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[7]:
+      return quick_setup_main_rep_target[7]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[8]:
+      return quick_setup_main_rep_target[8]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[9]:
+      return quick_setup_main_rep_target[9]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[10]:
+      return quick_setup_main_rep_target[10]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[11]:
+      return quick_setup_main_rep_target[11]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[12]:
+      return quick_setup_main_rep_target[12]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[13]:
+      return quick_setup_main_rep_target[13]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[14]:
+      return quick_setup_main_rep_target[14]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[15]:
+      return quick_setup_main_rep_target[15]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[16]:
+      return quick_setup_main_rep_target[16]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[17]:
+      return quick_setup_main_rep_target[17]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[18]:
+      return quick_setup_main_rep_target[18]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[19]:
+      return quick_setup_main_rep_target[19]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[20]:
+      return quick_setup_main_rep_target[20]
+    elif setup_main_intensity[w-1] == quick_setup_main_target_percent[21]:
+      return quick_setup_main_rep_target[21]
+    else:
+      print ("check")
 
 def setup_main_last_set_RIR (w):
-  count=1
-  while 1<=count<=21:
-
-      if setup_main_intensity[w - 1] == quick_setup_main_last_set_RIR_target[count - 1]:
-          setup_main_last_set_RIR = quick_setup_main_last_set_RIR_target[count - 1]
-          break
-      else :
-          count+=1
-  return setup_main_last_set_RIR
+  if setup_main_intensity[w-1] == quick_setup_main_target_percent[0]:
+    return quick_setup_main_last_set_RIR_target[0]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[1]:
+    return quick_setup_main_last_set_RIR_target[1]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[2]:
+    return quick_setup_main_last_set_RIR_target[2]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[3]:
+    return quick_setup_main_last_set_RIR_target[3]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[4]:
+    return quick_setup_main_last_set_RIR_target[4]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[5]:
+    return quick_setup_main_last_set_RIR_target[5]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[6]:
+    return quick_setup_main_last_set_RIR_target[6]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[7]:
+    return quick_setup_main_last_set_RIR_target[7]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[8]:
+    return quick_setup_main_last_set_RIR_target[8]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[9]:
+    return quick_setup_main_last_set_RIR_target[9]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[10]:
+    return quick_setup_main_last_set_RIR_target[10]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[11]:
+    return quick_setup_main_last_set_RIR_target[11]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[12]:
+    return quick_setup_main_last_set_RIR_target[12]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[13]:
+    return quick_setup_main_last_set_RIR_target[13]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[14]:
+    return quick_setup_main_last_set_RIR_target[14]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[15]:
+    return quick_setup_main_last_set_RIR_target[15]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[16]:
+    return quick_setup_main_last_set_RIR_target[16]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[17]:
+    return quick_setup_main_last_set_RIR_target[17]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[18]:
+    return quick_setup_main_last_set_RIR_target[18]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[19]:
+    return quick_setup_main_last_set_RIR_target[19]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[20]:
+    return quick_setup_main_last_set_RIR_target[20]
+  elif setup_main_intensity[w-1] == quick_setup_main_target_percent[21]:
+    return quick_setup_main_last_set_RIR_target[21]
+  else:
+    print ("check")
 
 #Aux
 
@@ -68,26 +137,100 @@ setup_aux_sets = quick_setup_aux_behavior_RIR_sets
 setup_aux_behavior_RIR_adjust = quick_setup_aux_behavior_RIR_adjust
 
 def setup_aux_reps_per_set (w):
-  count=1
-  while 1<=count<=21:
-
-      if setup_aux_intensity[w - 1] == quick_setup_aux_intensity[count - 1]:
-          setup_aux_lift_reps = quick_setup_aux_rep_target[count - 1]
-          break
-      else :
-          count+=1
-  return setup_aux_lift_reps
+    if setup_aux_intensity[w-1] == quick_setup_aux_target_percent[0]:
+      return quick_setup_aux_rep_target[0]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[1]:
+      return quick_setup_aux_rep_target[1]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[2]:
+      return quick_setup_aux_rep_target[2]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[3]:
+      return quick_setup_aux_rep_target[3]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[4]:
+      return quick_setup_aux_rep_target[4]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[5]:
+      return quick_setup_aux_rep_target[5]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[6]:
+      return quick_setup_aux_rep_target[6]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[7]:
+      return quick_setup_aux_rep_target[7]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[8]:
+      return quick_setup_aux_rep_target[8]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[9]:
+      return quick_setup_aux_rep_target[9]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[10]:
+      return quick_setup_aux_rep_target[10]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[11]:
+      return quick_setup_aux_rep_target[11]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[12]:
+      return quick_setup_aux_rep_target[12]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[13]:
+      return quick_setup_aux_rep_target[13]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[14]:
+      return quick_setup_aux_rep_target[14]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[15]:
+      return quick_setup_aux_rep_target[15]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[16]:
+      return quick_setup_aux_rep_target[16]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[17]:
+      return quick_setup_aux_rep_target[17]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[18]:
+      return quick_setup_aux_rep_target[18]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[19]:
+      return quick_setup_aux_rep_target[19]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[20]:
+      return quick_setup_aux_rep_target[20]
+    elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[21]:
+      return quick_setup_aux_rep_target[21]
+    else:
+      print ("check")
 
 def setup_aux_last_set_RIR (w):
-  count=1
-  while 1<=count<=21:
-
-      if setup_aux_intensity[w - 1] == quick_setup_aux_last_set_RIR_target[count - 1]:
-          setup_aux_rir = quick_setup_aux_last_set_RIR_target[count - 1]
-          break
-      else :
-          count+=1
-  return setup_aux_last_set_RIR
+  if setup_aux_intensity[w-1] == quick_setup_aux_target_percent[0]:
+    return quick_setup_aux_last_set_RIR_target[0]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[1]:
+    return quick_setup_aux_last_set_RIR_target[1]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[2]:
+    return quick_setup_aux_last_set_RIR_target[2]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[3]:
+    return quick_setup_aux_last_set_RIR_target[3]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[4]:
+    return quick_setup_aux_last_set_RIR_target[4]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[5]:
+    return quick_setup_aux_last_set_RIR_target[5]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[6]:
+    return quick_setup_aux_last_set_RIR_target[6]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[7]:
+    return quick_setup_aux_last_set_RIR_target[7]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[8]:
+    return quick_setup_aux_last_set_RIR_target[8]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[9]:
+    return quick_setup_aux_last_set_RIR_target[9]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[10]:
+    return quick_setup_aux_last_set_RIR_target[10]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[11]:
+    return quick_setup_aux_last_set_RIR_target[11]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[12]:
+    return quick_setup_aux_last_set_RIR_target[12]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[13]:
+    return quick_setup_aux_last_set_RIR_target[13]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[14]:
+    return quick_setup_aux_last_set_RIR_target[14]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[15]:
+    return quick_setup_aux_last_set_RIR_target[15]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[16]:
+    return quick_setup_aux_last_set_RIR_target[16]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[17]:
+    return quick_setup_aux_last_set_RIR_target[17]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[18]:
+    return quick_setup_aux_last_set_RIR_target[18]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[19]:
+    return quick_setup_aux_last_set_RIR_target[19]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[20]:
+    return quick_setup_aux_last_set_RIR_target[20]
+  elif setup_aux_intensity[w-1] == quick_setup_aux_target_percent[21]:
+    return quick_setup_aux_last_set_RIR_target[21]
+  else:
+    print ("check")
 
 ##############################______Untouched______##############################
 
@@ -235,8 +378,10 @@ def p_RIR_mainTM_weight(w):
 def p_RIR_main_weight(w):
   return (round(((p_RIR_mainTM_weight(w)) * (setup_main_intensity[w-1])), quick_setup_main_rounding))/100
 p_RIR_main_reps_per_set = setup_main_reps_per_set
-p_RIR_main_last_set_RIR_target = setup_main_last_set_RIR
-p_RIR_main_set_goal = setup_main_sets
+def p_RIR_main_last_set_RIR_target(w):
+  return setup_main_last_set_RIR(w)
+def p_RIR_main_set_goal(w):
+  return setup_main_sets[w-1]
 p_RIR_main_sets_completed = []
 p_RIR_main_RIR_on_last_set = []
 
@@ -288,8 +433,8 @@ p_RIR_aux_RIR_on_last_set = []
 ###########################_______Table______####################################################
 
 def table_main(w): 
-  return [p_RIR_main_weight(1),p_RIR_main_reps_per_set(w),p_RIR_main_last_set_RIR_target(w),p_RIR_main_set_goal(w)]
+  return [p_RIR_main_weight(w),p_RIR_main_reps_per_set(w),p_RIR_main_last_set_RIR_target(w),p_RIR_main_set_goal(w)]
 def table_aux(w): 
   return [p_RIR_aux_weight(1),p_RIR_aux_reps_per_set(w),p_RIR_aux_last_set_RIR_target(w),p_RIR_aux_set_goal(w)]
 #####################################################################################################################
-print (p_RIR_main_reps_per_set(0))
+print (p_RIR_main_set_goal(1))
